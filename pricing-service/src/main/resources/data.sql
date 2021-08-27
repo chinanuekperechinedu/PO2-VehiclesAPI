a404f8370fafe81effddaf2e6f81803bcb212a48
Chinanuekpere Chinedu <chinanuchinedu@gmail.com>
2021-08-27 12:31:44 +0100

    Initial Commit for Vehicles API

---
+++ pricing-service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO Price (id, vehicle_Id, currency, price) VALUES (1, 1, 'USD',4000);
+INSERT INTO Price (id, vehicle_Id, currency, price) VALUES (2, 2, 'NGN', 2500000);
+INSERT INTO Price (id, vehicle_Id, currency, price) VALUES (3, 3, 'CFN',7000);
+INSERT INTO Price (id, vehicle_Id, currency, price) VALUES (4, 4, 'GBP',8000);
+INSERT INTO Price (id, vehicle_Id, currency, price) VALUES (5, 5, 'EUR',3000);
